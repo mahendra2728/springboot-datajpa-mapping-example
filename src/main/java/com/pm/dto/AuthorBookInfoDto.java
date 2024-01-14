1abfce77a59d85deee2f497f59b76096e1dc79f2
Mahendra M <51850946+mahendra2728@users.noreply.github.com>
2024-01-14 14:00:50 +0530

    Update AuthorBookInfoDto.java

--- src/main/java/com/pm/dto/AuthorBookInfoDto.java
+++ src/main/java/com/pm/dto/AuthorBookInfoDto.java
@@ -1,1 +1,17 @@
-package com.pm.dto;import lombok.AllArgsConstructor;import lombok.Data;import lombok.NoArgsConstructor;import lombok.ToString;@Data@AllArgsConstructor@NoArgsConstructor@ToStringpublic class AuthorBookInfoDto {    private String authorName;    private String bookName;}
+package com.pm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AuthorBookInfoDto {
+
+    private String authorName;
+    private String bookName;
+
+}
